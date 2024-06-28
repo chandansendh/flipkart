ef5c79e9dd5ae1c7bad047674b4dca4fec6d35c7
Chandan Sendha <chandanchiku2002@gmail.com>
2024-06-28 21:29:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./Header";
+import { Data } from "./Data";
+import Card from "./Card";
+import Navbar from "./Navbar";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <div style={{ display: "flex", flexWrap: "wrap" }}>
+                {Data.map((item) => (
+                  <Card
+                    key={item.id}
+                    image={item.image}
+                    name={item.name}
+                    rating={item.rating}
+                    actualPrice={item.actualPrice}
+                    offerPrice={item.offerPrice}
+                  />
+                ))}
+              </div>
+            </>
+          }
+        />
+        <Route path="/cart" element={<Navbar text={"This is Cart Page"} />} />
+        <Route
+          path="/orders"
+          element={<Navbar text={"This is Orders Page"} />}
+        />
+      </Routes>
+    </>
   );
 }
 
